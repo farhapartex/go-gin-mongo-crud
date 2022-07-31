4e85eee38e1e0d443ef971b2770ff0943d21ca74
Md Nazmul Hasan <hasan08sust@gmail.com>
2022-08-01 00:29:10 +0600

    added service

---
+++ services/user.service.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	"github.com/farhapartex/go-gin-mongo-crud/models"
+)
+
+type UserService interface{
+	CreateUser(*models.User) error
+	GetUser(*string) (*models.User, error)
+	GetAll() ([]*models.User, error)
+}
